e5caf0f25ebeac55817a1ea52ea0faf90408839f
ferios08 <chb.firas@gmail.com>
2020-05-20 00:35:06 +0100

    fixing db autj

--- libs/database.js
+++ libs/database.js
@@ -3,13 +3,12 @@ const mongoose = require("mongoose");
 
 const {
     MONGO_URL,
-    MONGO_DB,
     MONGO_USER,
     MONGO_PASS,
     MONGO_PORT=27017
 } = process.env;
 
-const DBHost = `${MONGO_USER}:${MONGO_PASS}@${MONGO_URL}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+const DBHost = `${MONGO_USER}:${MONGO_PASS}@${MONGO_URL}:${MONGO_PORT}?authSource=admin`;
 
 
 //Database Connection
